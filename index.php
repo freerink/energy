ad29b7531351227d01a09c7c75a7661f6be3cc35
Frank Reerink <nospam@reerink.name>
2022-03-21 22:01:35 +0100

    Do some queries

--- index.php
+++ index.php
@@ -45,7 +45,26 @@ if( ! isset($_SERVER['HTTP_AUTHORIZATION']) ) {
 				echo "Invalid token\n";
 				http_response_code(403);
 			} else {
-				echo "POST";
+				echo "DB connect\n";
+				$link = mysqli_connect(getDbHost($energyConfig), getDbUser($energyConfig), getDbPassword($energyConfig), getDbName($energyConfig)) or die ('Could not connect to database: ' . mysql_error());
+				$current = $_REQUEST['current'];
+				$datetime = $_REQUEST['datetime'];
+				echo "current: ${current}\n";
+				echo "datetime: ${datetime}\n";
+				$query = "insert into current_max (current, datetime) values (${current}, '${datetime}');select min(current) from current_max; select max(current) from current_max;select * from current_max;";
+				if ( mysqli_multi_query($link, $query) ) {
+					echo "First query OK\n";
+					do {
+						if ( $result = mysqli_store_result($link) ) {
+							while( $row = mysqli_fetch_row($result) ) {
+								echo "ROW: ";
+								print_r($row);
+							}
+						}
+					} while ( mysqli_more_results($link) && mysqli_next_result($link) );
+				} else {
+					echo "First query failed\n";
+				}
 			}
 		}
 	}
